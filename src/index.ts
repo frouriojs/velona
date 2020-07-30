7fa8fad3de2efc108165db4561a0a3980a6dccb5
solufa <solufa2020@gmail.com>
2020-07-30 20:25:53 +0900

    fix: rename params to args

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 type Deps<T extends Record<string, any>> = {
   [P in keyof T]: T[P] extends { _velona: boolean }
-    ? (...deps: Parameters<T[P]>) => ReturnType<T[P]>
+    ? (...args: Parameters<T[P]>) => ReturnType<T[P]>
     : T[P]
 }
 
 export const depend = <T extends Record<string, any>, U extends [] | [any, ...any[]], V>(
   dependencies: T,
-  cb: (deps: Deps<T>, ...params: U) => V
+  cb: (deps: Deps<T>, ...args: U) => V
 ) => {
   const fn = (...args: U) => cb(dependencies, ...args)
   fn._velona = true
