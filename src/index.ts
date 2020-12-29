c895416602743dcbf85fd71287b223433b29e42d
solufa <solufa2020@gmail.com>
2020-12-30 07:00:48 +0900

    feat: define Injectable type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+export type Injectable<T, U extends any[], V> = {
+  (...args: U): V
+  inject: (deps: Partial<T> | ((d: T) => Partial<T>)) => Injectable<T, U, V>
+}
+
 export const depend = <T extends Record<string, any>, U extends any[], V>(
   dependencies: T,
   cb: (deps: T, ...args: U) => V
-) => {
+): Injectable<T, U, V> => {
   const fn = (...args: U) => cb(dependencies, ...args)
   fn.inject = (deps: Partial<T> | ((d: T) => Partial<T>)) =>
     typeof deps === 'function'
